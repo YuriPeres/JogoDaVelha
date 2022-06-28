34698fe8076f06d663e635d1633db9edf90897a3
Yuri Peres <yuriperes92@gmail.com>
2022-06-28 14:46:44 -0300

    mudei a main de lugar

---
+++ src/jogodavelha/JogoDaVelha.java
@@ -0,0 +1,14 @@
+package jogodavelha;
+
+import jogodavelha.view.Tela;
+
+import java.util.Random;
+
+public class JogoDaVelha {
+
+    public static void main(String[] args) {
+        Tela interfaceDoJogo = new Tela();
+
+    }
+
+}
